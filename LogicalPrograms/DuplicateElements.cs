85a3090f39f4e71403812cf31f69a553256f4597
Shivani21-hude <shivani21hude@gmail.com>
2023-03-22 15:56:59 +0530

    [Shivani]-To count total number of duplicate elements in an array

---
+++ LogicalPrograms/DuplicateElements.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class DuplicateElements
+    {
+        //  take input as array elements to find duplicate numbers
+        public static void DuplicateNumbers()
+        {
+            Console.WriteLine("Enter a number ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+            int[] arr = new int[number];
+            Console.WriteLine("Enter integer numbers ");
+
+            for(int i=0; i < number; i++)
+            {
+                arr[i] =Convert.ToInt32(Console.ReadLine());
+            }
+
+            for(int j=0; j<number; j++)
+            {
+                for(int k=j+1; k<number; k++)
+                {
+                    if (arr[k] == arr[j])
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine("Duplicate elements " + count);
+        }
+       
+    }
+}
---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Choose from following options : \n 1) To count total number of duplicate elements in an array.");
+                int option=Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        DuplicateElements.DuplicateNumbers();
+                        break;  
+                }
+            }
+                      
+        }
+    }
+}
